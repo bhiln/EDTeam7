4524f1ac0468159baa9af44fd8fb66fe88294563
bhiln <brhiln@gmail.com>
2014-09-11 13:10:01 -0400

    Added sensor gui slider
    still need to pass the value to the sensor function

--- Simulation/MS1_MatlabSim/SensorSim/editSensor.m
+++ Simulation/MS1_MatlabSim/SensorSim/editSensor.m
@@ -1,35 +1,35 @@
-function varargout = editSensorData(varargin)
-% EDITSENSORDATA MATLAB code for editSensorData.fig
-%      EDITSENSORDATA, by itself, creates a new EDITSENSORDATA or raises the existing
+function varargout = editSensor(varargin)
+% EDITSENSOR MATLAB code for editSensor.fig
+%      EDITSENSOR, by itself, creates a new EDITSENSOR or raises the existing
 %      singleton*.
 %
-%      H = EDITSENSORDATA returns the handle to a new EDITSENSORDATA or the handle to
+%      H = EDITSENSOR returns the handle to a new EDITSENSOR or the handle to
 %      the existing singleton*.
 %
-%      EDITSENSORDATA('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in EDITSENSORDATA.M with the given input arguments.
+%      EDITSENSOR('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in EDITSENSOR.M with the given input arguments.
 %
-%      EDITSENSORDATA('Property','Value',...) creates a new EDITSENSORDATA or raises the
+%      EDITSENSOR('Property','Value',...) creates a new EDITSENSOR or raises the
 %      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before editSensorData_OpeningFcn gets called.  An
+%      applied to the GUI before editSensor_OpeningFcn gets called.  An
 %      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to editSensorData_OpeningFcn via varargin.
+%      stop.  All inputs are passed to editSensor_OpeningFcn via varargin.
 %
 %      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
 %      instance to run (singleton)".
 %
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
-% Edit the above text to modify the response to help editSensorData
+% Edit the above text to modify the response to help editSensor
 
-% Last Modified by GUIDE v2.5 10-Sep-2014 13:12:32
+% Last Modified by GUIDE v2.5 11-Sep-2014 10:21:38
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @editSensorData_OpeningFcn, ...
-                   'gui_OutputFcn',  @editSensorData_OutputFcn, ...
+                   'gui_OpeningFcn', @editSensor_OpeningFcn, ...
+                   'gui_OutputFcn',  @editSensor_OutputFcn, ...
                    'gui_LayoutFcn',  [] , ...
                    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
@@ -44,26 +44,26 @@ end
 % End initialization code - DO NOT EDIT
 
 
-% --- Executes just before editSensorData is made visible.
-function editSensorData_OpeningFcn(hObject, eventdata, handles, varargin)
+% --- Executes just before editSensor is made visible.
+function editSensor_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to editSensorData (see VARARGIN)
+% varargin   command line arguments to editSensor (see VARARGIN)
 
-% Choose default command line output for editSensorData
+% Choose default command line output for editSensor
 handles.output = hObject;
 
 % Update handles structure
 guidata(hObject, handles);
 
-% UIWAIT makes editSensorData wait for user response (see UIRESUME)
+% UIWAIT makes editSensor wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
 
 % --- Outputs from this function are returned to the command line.
-function varargout = editSensorData_OutputFcn(hObject, eventdata, handles) 
+function varargout = editSensor_OutputFcn(hObject, eventdata, handles) 
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -71,14 +71,7 @@ function varargout = editSensorData_OutputFcn(hObject, eventdata, handles)
 
 % Get default command line output from handles structure
 varargout{1} = handles.output;
-
-
-% --- Executes on button press in pushbutton1.
-function pushbutton1_Callback(hObject, eventdata, handles)
-% hObject    handle to pushbutton1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-obj.setCurrentDistance(9)
+varargout{2} = hObject;
 
 
 % --- Executes on slider movement.
@@ -89,7 +82,9 @@ function slider1_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'Value') returns position of slider
 %        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
-
+num = get(hObject, 'Value');
+set(handles.edit2, 'String', num);
+Sensor(5).setCurrentDistance(num);
 
 % --- Executes during object creation, after setting all properties.
 function slider1_CreateFcn(hObject, eventdata, handles)
@@ -100,4 +95,27 @@ function slider1_CreateFcn(hObject, eventdata, handles)
 % Hint: slider controls usually have a light gray background.
 if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+
+
+
+function edit2_Callback(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit2 as text
+%        str2double(get(hObject,'String')) returns contents of edit2 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
 end
