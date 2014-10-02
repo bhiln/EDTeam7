9e395b53cc650e4f28a1d139f4b3bcdea765816c
bhiln <brhiln@gmail.com>
2014-10-02 09:50:57 -0400

    updated dummy motor control message

--- Simulation/ARMSim/ARMController.m
+++ Simulation/ARMSim/ARMController.m
@@ -25,7 +25,7 @@ classdef ARMController < handle
             ntimes_Char = bin2dec(sprintf('%s', dec2bin(ntimes, 8)));
 
             % Add the silly terminator and send the string over WiFly
-            str = sprintf('%s%s%s%s%s%s',ntimes_Char, bin2dec(sprintf('%s', dec2bin(50, 8))), bin2dec(sprintf('%s', dec2bin(49, 8))), bin2dec(sprintf('%s', dec2bin(255, 8))), bin2dec(sprintf('%s', dec2bin(0, 8))));
+            str = sprintf('%s%s%s%s%s%s',ntimes_Char, bin2dec(sprintf('%s', dec2bin(50, 8))), bin2dec(sprintf('%s', dec2bin(35, 8))), bin2dec(sprintf('%s', dec2bin(36, 8))), bin2dec(sprintf('%s', dec2bin(255, 8))), bin2dec(sprintf('%s', dec2bin(0, 8))));
             fprintf('ARMSim timer callback: Sending %s over WiFly\n',str);
             fwrite(ioWiFly,str);
             
