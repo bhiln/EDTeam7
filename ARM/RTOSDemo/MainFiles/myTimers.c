85a746dfc8d2a6197c63d1efa3537e0964dee050
Igor Janjic <i.janjic@live.com>
2014-10-10 16:55:37 -0400

    Put sensors into one task and wrote base code for rover localization

--- ARM/RTOSDemo/MainFiles/myTimers.c
+++ ARM/RTOSDemo/MainFiles/myTimers.c
@@ -50,7 +50,7 @@ void startTimerLocate(structLocate* dataLocate) {
 	if (sizeof(long) != sizeof(structLocate*))
 		VT_HANDLE_FATAL_ERROR(0);
 
-	xTimerHandle timerHandleLocate = xTimerCreate((const signed char*)locateName, LOCATE_WRITE_RATE_BASE, pdTRUE, (void*)dataLocate, timerCallbackLocate);
+	xTimerHandle timerHandleLocate = xTimerCreate((const signed char*)taskNameLocate, LOCATE_WRITE_RATE_BASE, pdTRUE, (void*)dataLocate, timerCallbackLocate);
 	if (timerHandleLocate == NULL)
 		VT_HANDLE_FATAL_ERROR(0);
 	else
@@ -77,7 +77,7 @@ void startTimerCommand(structCommand* dataCommand)
 	if (sizeof(long) != sizeof(structCommand*))
 		VT_HANDLE_FATAL_ERROR(0);
 
-	xTimerHandle timerHandleCommand = xTimerCreate((const signed char*)commandName, COMMAND_WRITE_RATE_BASE, pdTRUE, (void*)dataCommand, timerCallbackCommand);
+	xTimerHandle timerHandleCommand = xTimerCreate((const signed char*)taskNameCommand, COMMAND_WRITE_RATE_BASE, pdTRUE, (void*)dataCommand, timerCallbackCommand);
 	if (timerHandleCommand == NULL)
 		VT_HANDLE_FATAL_ERROR(0);
 	else
@@ -87,287 +87,30 @@ void startTimerCommand(structCommand* dataCommand)
 	}
 }
 
-// Sensor IR00.
-void timerCallbackIR00(xTimerHandle pxTimer)
+void timerCallbackSensor(xTimerHandle pxTimer)
 {
 	if (pxTimer == NULL)
 		VT_HANDLE_FATAL_ERROR(0);
 	else
 	{
 		structSensor *ptr = (structSensor*) pvTimerGetTimerID(pxTimer);
-		if (sendTimerMsgSensor(ptr, msgTypeTimerIR00, IR00_WRITE_RATE_BASE, 0) == errQUEUE_FULL)
+		if (sendTimerMsgSensor(ptr, msgSensorTimer, SENSOR_WRITE_RATE_BASE, 0) == errQUEUE_FULL)
 			VT_HANDLE_FATAL_ERROR(0);
 	}
 }
 
-// Sensor IR01
-void timerCallbackIR01(xTimerHandle pxTimer)
-{
-	if (pxTimer == NULL)
-		VT_HANDLE_FATAL_ERROR(0);
-	else
-	{
-		structSensor *ptr = (structSensor*) pvTimerGetTimerID(pxTimer);
-		if (sendTimerMsgSensor(ptr, msgTypeTimerIR01, IR01_WRITE_RATE_BASE, 0) == errQUEUE_FULL)
-			VT_HANDLE_FATAL_ERROR(0);
-	}
-}
-
-// Sensor IR10.
-void timerCallbackIR10(xTimerHandle pxTimer)
-{
-	if (pxTimer == NULL)
-		VT_HANDLE_FATAL_ERROR(0);
-	else
-	{
-		structSensor *ptr = (structSensor*) pvTimerGetTimerID(pxTimer);
-		if (sendTimerMsgSensor(ptr, msgTypeTimerIR10, IR10_WRITE_RATE_BASE, 0) == errQUEUE_FULL)
-			VT_HANDLE_FATAL_ERROR(0);
-	}
-}
-
-// Sensor IR11.
-void timerCallbackIR11(xTimerHandle pxTimer)
-{
-	if (pxTimer == NULL)
-		VT_HANDLE_FATAL_ERROR(0);
-	else
-	{
-		structSensor *ptr = (structSensor*) pvTimerGetTimerID(pxTimer);
-		if (sendTimerMsgSensor(ptr, msgTypeTimerIR11, IR11_WRITE_RATE_BASE, 0) == errQUEUE_FULL)
-			VT_HANDLE_FATAL_ERROR(0);
-	}
-}
-
-// Sensor IR20.
-void timerCallbackIR20(xTimerHandle pxTimer)
-{
-	if (pxTimer == NULL)
-		VT_HANDLE_FATAL_ERROR(0);
-	else
-	{
-		structSensor *ptr = (structSensor*) pvTimerGetTimerID(pxTimer);
-		if (sendTimerMsgSensor(ptr, msgTypeTimerIR20, IR20_WRITE_RATE_BASE, 0) == errQUEUE_FULL)
-			VT_HANDLE_FATAL_ERROR(0);
-	}
-}
-
-// Sensor IR21.
-void timerCallbackIR21(xTimerHandle pxTimer)
-{
-	if (pxTimer == NULL)
-		VT_HANDLE_FATAL_ERROR(0);
-	else
-	{
-		structSensor *ptr = (structSensor*) pvTimerGetTimerID(pxTimer);
-		if (sendTimerMsgSensor(ptr, msgTypeTimerIR21, IR21_WRITE_RATE_BASE, 0) == errQUEUE_FULL)
-			VT_HANDLE_FATAL_ERROR(0);
-	}
-}
-
-// Sensor IR30.
-void timerCallbackIR30(xTimerHandle pxTimer)
-{
-	if (pxTimer == NULL)
-		VT_HANDLE_FATAL_ERROR(0);
-	else
-	{
-		structSensor *ptr = (structSensor*) pvTimerGetTimerID(pxTimer);
-		if (sendTimerMsgSensor(ptr, msgTypeTimerIR30, IR30_WRITE_RATE_BASE, 0) == errQUEUE_FULL)
-			VT_HANDLE_FATAL_ERROR(0);
-	}
-}
-
-// Sensor IR31.
-void timerCallbackIR31(xTimerHandle pxTimer)
-{
-	if (pxTimer == NULL)
-		VT_HANDLE_FATAL_ERROR(0);
-	else
-	{
-		structSensor *ptr = (structSensor*) pvTimerGetTimerID(pxTimer);
-		if (sendTimerMsgSensor(ptr, msgTypeTimerIR31, IR31_WRITE_RATE_BASE, 0) == errQUEUE_FULL)
-			VT_HANDLE_FATAL_ERROR(0);
-	}
-}
-
-// Sensor IR40.
-void timerCallbackIR40(xTimerHandle pxTimer)
-{
-	if (pxTimer == NULL)
-		VT_HANDLE_FATAL_ERROR(0);
-	else
-	{
-		structSensor *ptr = (structSensor*) pvTimerGetTimerID(pxTimer);
-		if (sendTimerMsgSensor(ptr, msgTypeTimerIR40, IR40_WRITE_RATE_BASE, 0) == errQUEUE_FULL)
-			VT_HANDLE_FATAL_ERROR(0);
-	}
-}
-
-// Sensor IR41.
-void timerCallbackIR41(xTimerHandle pxTimer)
-{
-	if (pxTimer == NULL)
-		VT_HANDLE_FATAL_ERROR(0);
-	else
-	{
-		structSensor *ptr = (structSensor*) pvTimerGetTimerID(pxTimer);
-		if (sendTimerMsgSensor(ptr, msgTypeTimerIR41, IR41_WRITE_RATE_BASE, 0) == errQUEUE_FULL)
-			VT_HANDLE_FATAL_ERROR(0);
-	}
-}
-
-// Sensor AC00.
-void timerCallbackAC00(xTimerHandle pxTimer)
-{
-	if (pxTimer == NULL)
-		VT_HANDLE_FATAL_ERROR(0);
-	else
-	{
-		structSensor *ptr = (structSensor*) pvTimerGetTimerID(pxTimer);
-		if (sendTimerMsgSensor(ptr, msgTypeTimerAC00, AC00_WRITE_RATE_BASE, 0) == errQUEUE_FULL)
-			VT_HANDLE_FATAL_ERROR(0);
-	}
-}
-
-void startTimerSensor(uint8_t sensor, structSensor* dataSensor)
+void startTimerSensor(structSensor* dataSensor)
 {
 	if (sizeof(long) != sizeof(structSensor*))
 		VT_HANDLE_FATAL_ERROR(0);
 
-	switch(sensor)
+	xTimerHandle timerHandleSensor = xTimerCreate((const signed char*)taskNameSensor, SENSOR_WRITE_RATE_BASE, pdTRUE, (void*)dataSensor, timerCallbackSensor);
+	if (timerHandleSensor == NULL)
+		VT_HANDLE_FATAL_ERROR(0);
+	else
 	{
-		case sensorIR00:
-		{
-			xTimerHandle timerHandleIR00 = xTimerCreate((const signed char*)sensorNameIR00, IR00_WRITE_RATE_BASE, pdTRUE, (void*)dataSensor, timerCallbackIR00);
-			if (timerHandleIR00 == NULL)
-				VT_HANDLE_FATAL_ERROR(0);
-			else
-			{
-				if (xTimerStart(timerHandleIR00, 0) != pdPASS)
-					VT_HANDLE_FATAL_ERROR(0);
-			}
-			break;
-		}
-		case sensorIR01:
-		{
-			xTimerHandle timerHandleIR01 = xTimerCreate((const signed char*)sensorNameIR01, IR01_WRITE_RATE_BASE, pdTRUE, (void*)dataSensor, timerCallbackIR01);
-			if (timerHandleIR01 == NULL)
-				VT_HANDLE_FATAL_ERROR(0);
-			else
-			{
-				if (xTimerStart(timerHandleIR01, 0) != pdPASS)
-					VT_HANDLE_FATAL_ERROR(0);
-			}
-			break;
-		}
-		case sensorIR10:
-		{
-			xTimerHandle timerHandleIR10 = xTimerCreate((const signed char*)sensorNameIR10, IR10_WRITE_RATE_BASE, pdTRUE, (void*)dataSensor, timerCallbackIR10);
-			if (timerHandleIR10 == NULL)
-				VT_HANDLE_FATAL_ERROR(0);
-			else
-			{
-				if (xTimerStart(timerHandleIR10, 0) != pdPASS)
-					VT_HANDLE_FATAL_ERROR(0);
-			}
-			break;
-		}
-		case sensorIR11:
-		{
-			xTimerHandle timerHandleIR11 = xTimerCreate((const signed char*)sensorNameIR11, IR11_WRITE_RATE_BASE, pdTRUE, (void*)dataSensor, timerCallbackIR11);
-			if (timerHandleIR11 == NULL)
-				VT_HANDLE_FATAL_ERROR(0);
-			else
-			{
-				if (xTimerStart(timerHandleIR11, 0) != pdPASS)
-					VT_HANDLE_FATAL_ERROR(0);
-			}
-			break;
-		}
-		case sensorIR20:
-		{
-			xTimerHandle timerHandleIR20 = xTimerCreate((const signed char*)sensorNameIR20, IR20_WRITE_RATE_BASE, pdTRUE, (void*)dataSensor, timerCallbackIR20);
-			if (timerHandleIR20 == NULL)
-				VT_HANDLE_FATAL_ERROR(0);
-			else
-			{
-				if (xTimerStart(timerHandleIR20, 0) != pdPASS)
-					VT_HANDLE_FATAL_ERROR(0);
-			}
-			break;
-		}
-		case sensorIR21:
-		{
-			xTimerHandle timerHandleIR21 = xTimerCreate((const signed char*)sensorNameIR21, IR21_WRITE_RATE_BASE, pdTRUE, (void*)dataSensor, timerCallbackIR21);
-			if (timerHandleIR21 == NULL)
-				VT_HANDLE_FATAL_ERROR(0);
-			else
-			{
-				if (xTimerStart(timerHandleIR21, 0) != pdPASS)
-					VT_HANDLE_FATAL_ERROR(0);
-			}
-			break;
-		}
-		case sensorIR30:
-		{
-			xTimerHandle timerHandleIR30 = xTimerCreate((const signed char*)sensorNameIR30, IR30_WRITE_RATE_BASE, pdTRUE, (void*)dataSensor, timerCallbackIR30);
-			if (timerHandleIR30 == NULL)
-				VT_HANDLE_FATAL_ERROR(0);
-			else
-			{
-				if (xTimerStart(timerHandleIR30, 0) != pdPASS)
-					VT_HANDLE_FATAL_ERROR(0);
-			}
-			break;
-		}
-		case sensorIR31:
-		{
-			xTimerHandle timerHandleIR31 = xTimerCreate((const signed char*)sensorNameIR31, IR31_WRITE_RATE_BASE, pdTRUE, (void*)dataSensor, timerCallbackIR31);
-			if (timerHandleIR31 == NULL)
-				VT_HANDLE_FATAL_ERROR(0);
-			else
-			{
-				if (xTimerStart(timerHandleIR31, 0) != pdPASS)
-					VT_HANDLE_FATAL_ERROR(0);
-			}
-			break;
-		}
-		case sensorIR40:
-		{
-			xTimerHandle timerHandleIR40 = xTimerCreate((const signed char*)sensorNameIR40, IR40_WRITE_RATE_BASE, pdTRUE, (void*)dataSensor, timerCallbackIR40);
-			if (timerHandleIR40 == NULL)
-				VT_HANDLE_FATAL_ERROR(0);
-			else
-			{
-				if (xTimerStart(timerHandleIR40, 0) != pdPASS)
-					VT_HANDLE_FATAL_ERROR(0);
-			}
-			break;
-		}
-		case sensorIR41:
-		{
-			xTimerHandle timerHandleIR41 = xTimerCreate((const signed char*)sensorNameIR41, IR41_WRITE_RATE_BASE, pdTRUE, (void*)dataSensor, timerCallbackIR41);
-			if (timerHandleIR41 == NULL)
-				VT_HANDLE_FATAL_ERROR(0);
-			else
-			{
-				if (xTimerStart(timerHandleIR41, 0) != pdPASS)
-					VT_HANDLE_FATAL_ERROR(0);
-			}
-			break;
-		}
-		case sensorAC00:
-		{
-			xTimerHandle timerHandleAC00 = xTimerCreate((const signed char*)sensorNameAC00, AC00_WRITE_RATE_BASE, pdTRUE, (void*)dataSensor, timerCallbackAC00);
-			if (timerHandleAC00 == NULL)
-				VT_HANDLE_FATAL_ERROR(0);
-			else
-			{
-				if (xTimerStart(timerHandleAC00, 0) != pdPASS)
-					VT_HANDLE_FATAL_ERROR(0);
-			}
-			break;
-		}
+		if (xTimerStart(timerHandleSensor, 0) != pdPASS)
+			VT_HANDLE_FATAL_ERROR(0);
 	}
 }
+
--- ARM/RTOSDemo/MainFiles/taskLocate.h
+++ ARM/RTOSDemo/MainFiles/taskLocate.h
@@ -7,18 +7,15 @@
 // Maximum length of a message that can be received by any IR task.
 #define maxLenLocate (sizeof(portTickType))
 
-// Types of messages this task will receive.
-#define msgTypeIR00	0
-#define msgTypeIR01 	1
-#define msgTypeIR10	2
-#define msgTypeIR11 	3
-#define msgTypeIR20	4
-#define msgTypeIR21 	5
-#define msgTypeIR30	6
-#define msgTypeIR31 	7
-#define msgTypeIR40	8
-#define msgTypeIR41 	9
-#define msgTypeAC00	10
+#define MAX_COMMAND_HISTORY 100
+#define NUMBER_SENSORS 		11
+
+#define roverFront	0
+#define roverRight 	1
+#define roverBack  	2
+#define roverLeft  	3
+
+signed char taskNameLocate[] = "Locate";
 
 // Structure used to pass parameters to the task.
 typedef struct __structLocate
@@ -27,11 +24,82 @@ typedef struct __structLocate
 	xQueueHandle inQ;
 } structLocate;
 
-signed char locateName[] = "Locate";
+// Struct representing a command.
+typedef struct __Command
+{
+	uint8_t commandType;
+	uint8_t commandValue;
+	uint8_t commandSpeed;
+} Command;
+
+// Struct containing the distance and angle for an object on a specific side of
+// the rover.
+typedef struct __Object
+{
+	int16_t distance;
+	int16_t angle;
+} Object;
+
+// Enum of the possible states of rover motion.
+typedef enum 
+{
+	stopped,
+	emergencyStoppped,
+	movingForward,
+	movingBackward,
+	turningLeft,
+	turningRight,
+	unknown
+} StateMotion;
+
+// Enum of the possible states of rover localization.
+typedef enum
+{
+	none,
+	roam,
+	followWall,
+	turnCornerLeft,
+	turnCornerRight,
+	turnAround,
+	moveTowardObject,
+	moveTowardRamp,
+	alignWithRamp,
+	moveOverRamp
+} StateGoal;
+
+// List of sensor distance values.
+uint16_t locateData[NUMBER_SENSORS];
+
+// History of the past commands.
+Command commandHistory[MAX_COMMAND_HISTORY];
+
+// List containing front, right, back, and left rover objects.
+Object sideObject[3];
+
+// Closest ramp.
+Object ramp;
+
+// State machine.
+StateMotion curStateMotion = stopped;
+StateGoal curStateGoal = none;
 
 // Sensor sensor.
 void startTaskLocate(structLocate* dataLocate, unsigned portBASE_TYPE uxPriority, structLCD* dataLCD);
 portBASE_TYPE sendTimerMsgLocate(structLocate* dataLocate, portTickType ticksElapsed, portTickType ticksToBlock);
 portBASE_TYPE sendValueMsgLocate(structLocate* dataLocate, uint8_t msgType, uint16_t value, portTickType ticksToBlock);
+
+void locateRover();
+void updateRamp();
+
+void execNone();
+void execRoam();
+void execFollowWall();
+void execTurnCornerLeft();
+void execTurnCornerRight();
+void execTurnAround();
+void execMoveTowardObject();
+void execMoveTowardRamp();
+void execAlignWithRamp();
+void execMoveOverRamp();
 
 #endif
